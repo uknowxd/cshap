d7be8020d89e58fdbdf5112ebf2f06e284e14e89
uknowxd <74955335+uknowxd@users.noreply.github.com>
2023-10-22 14:49:23 +0700

    [add] week-11

--- project/Program.cs
+++ project/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Manager m = new Manager();
+		m.showMenu();
+		//Console.WriteLine("Hello World");
+	}
+}
+
+class Manager
+{
+    public string[] Products;
+	public void showMenu()
+	{
+		int num = 0;
+		string userChoice = string.Empty;
+		do
+		{
+			Console.WriteLine("Select the number of your choice: ");
+			Console.WriteLine("1: Add a game");
+			Console.WriteLine("2: Remove a game");
+			Console.WriteLine("3: List games");
+			Console.WriteLine("4: Save games");
+			Console.WriteLine("5: Read games from file");
+			Console.WriteLine("6: Quit");
+			Console.Write("Enter the number of your choice: ");
+			if(num == 6)
+				break;
+			else
+				userChoice = Console.ReadLine();
+		}
+		while (Int32.TryParse(userChoice, out num)); //checks to see if the input is a number
+            Console.WriteLine(userChoice);
+		if (userChoice == "6")
+		{
+			Environment.Exit(0);
+		}
+		Console.WriteLine("Choice = " + userChoice);
+		if (userChoice == "1")
+		{
+            Console.WriteLine("test");
+		//Some code that will allow me to enter info on some game
+		// it will then display the menu again after all questions are answered.
+		}
+	}
+
+    public void listProduct()
+    {
+
+    }
+}
---
+++ week-11/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Movie m = new Movie();
+        m.MainMenu();
+    }
+}
+
+class Movie
+{
+    public string watchMovie;
+    public string roundTimes;
+    public void MainMenu()
+    {
+        Console.Clear();
+        Movie m = new Movie();
+        Console.WriteLine("Welcome To Chanwit Cineplex");
+        Console.WriteLine("Please Selected Movie");
+        Console.WriteLine("1.Sapalur");
+        Console.WriteLine("2.The Marvel");
+        Console.WriteLine("3.TeeYod");
+        Console.WriteLine("Press Q = Exit Program");
+        Console.WriteLine(" -------------------- ");
+        Console.Write("Please Enter Your Input Selected : ");
+        string userChoice = Console.ReadLine().ToUpper();
+        Console.WriteLine(" -------------------- ");
+        m.initialChoice(userChoice);
+    }
+
+    public void initialChoice(string Choice)
+    {
+        Movie m = new Movie();
+        Console.Clear();
+        if (Choice == "1" || Choice == "2" || Choice == "3"){
+            m.getNameMovie(Int32.Parse(Choice));
+            m.RoundTimes();
+        }else{
+            Console.Clear();
+            Console.WriteLine(" ---------------------------------- ");
+            Console.WriteLine("The menu you selected was not found.");
+            Console.WriteLine(" ---------------------------------- ");
+            Console.WriteLine("Press Q = Exit, H = Back To Menu");
+            string NotfoundChoice = Console.ReadLine().ToUpper();
+            if ( NotfoundChoice == "H"){
+                m.MainMenu();
+            }else if (NotfoundChoice == "Q"){
+                m.initialChoice("Q");
+            }
+        }
+    }
+
+    public void RoundTimes()
+    {
+        Movie m = new Movie();
+        Console.Clear();
+        Console.WriteLine(" ---------------------------------- ");
+        Console.WriteLine("Your Selected Movie : {0}", watchMovie);
+        Console.WriteLine("Please Selected Movie Round Times");
+        Console.WriteLine(" ---------------------------------- ");
+        Console.WriteLine("1. 10:00 - 12:00");
+        Console.WriteLine("2. 12:00 - 14:00");
+        Console.WriteLine("3. 14:00 - 16:00");
+        Console.WriteLine("Press Q = Exit, H = Back To Menu");
+        Console.WriteLine(" ---------------------------------- ");
+        Console.Write("Please Enter Your Input Selected : ");
+        string userRoundTimes = Console.ReadLine().ToUpper();
+        Console.WriteLine(" ---------------------------------- ");
+        if (userRoundTimes == "1" || userRoundTimes == "2" || userRoundTimes == "3"){
+            m.getRoundTimes(Int32.Parse(userRoundTimes), watchMovie);
+            Console.Clear();
+            m.Successfully();
+        }else{
+            m.initialChoice(null);
+        }
+    }
+
+    public void getNameMovie(int MoviesId)
+    {
+        switch (MoviesId)
+        {
+            case 1:
+                watchMovie = "Sapalur";
+                break;
+            case 2:
+                watchMovie = "The Marvel";
+                break;
+            case 3:
+               watchMovie = "TeeYod";
+                break;
+        }
+    }
+
+    public void getRoundTimes(int TimesId, string watchMovieName)
+    {
+        watchMovie = watchMovieName;
+        switch (TimesId)
+        {
+            case 1:
+                roundTimes = "10:00 - 12:00";
+                break;
+            case 2:
+                roundTimes = "12:00 - 14:00";
+                break;
+            case 3:
+                roundTimes = "14:00 - 16:00";
+                break;
+        }
+    }
+
+    public void Successfully()
+    {
+        Console.WriteLine(" ===== Successfully reserved =====");
+        Console.WriteLine("Your Watch Movie : {0}", watchMovie);
+        Console.WriteLine("Your Watch Round Times : {0}", roundTimes);
+        Console.WriteLine("==================================");
+    }
+
+
+}
